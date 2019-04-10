e576dc17b9c395bdabf9edc6309e6d3f7015a1d7
gourav-j <gjhunjhunuwala.96@gmail.com>
2019-04-10 16:18:35 +0530

    Updated tests.py

--- pms/tests.py
+++ pms/tests.py
@@ -1,13 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 from .models import UserProfileInfo
 # Create your tests here.
 
 #Test Case for model
 class UserTest(TestCase):
-	def create_user(self, username="test1", password="12345678", first_name="test", last_name="test", dob="2014-02-06", gender="m", country="India", state="Ktk", city="BLR", address="344 Ns Road", pincode=723456, email_id="test1@gmail.com", mobile_no="4568796512"):
-		return UserProfileInfo.objects.create(username=username,password=password,first_name=first_name,last_name=last_name,dob=dob,gender=gender,country=country,state=state,city=city,address=address,pincode=pincode,email_id=email_id,mobile_no=mobile_no)
+	def add_user(self):
+		return User.objects.create_user("test1",password="12345678")
+	def register_user(self,user):
+		return UserProfileInfo.objects.create(user=user,first_name="test", last_name="test", dob="2014-02-06", gender="m", country="India", state="Ktk", city="BLR", address="344 Ns Road", pincode=723456, email_id="test1@gmail.com", mobile_no="4568796512")
 
 	def test_creation(self):
-		w=self.create_user()
-		self.assertTrue(isinstance(w,UserProfileInfo))
+		w=self.add_user()
+		g=self.register_user(w)
+		self.assertTrue(isinstance(g,UserProfileInfo))
