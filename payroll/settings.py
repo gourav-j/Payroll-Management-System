b75c76a1b608a1247689dbc5c9b3697b2e2708fc
gourav-j <gjhunjhunuwala.96@gmail.com>
2019-04-10 15:43:57 +0530

    Updated requirements.txt file

--- payroll/settings.py
+++ payroll/settings.py
@@ -124,9 +124,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
